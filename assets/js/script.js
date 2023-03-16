747a353e5715f3d5a695f34875812ac8749acde9
PatSvedberg <pat.svedberg@gmail.com>
2023-03-16 15:59:35 +0000

    Change answer buttons with divs and testing some java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 let question = document.getElementById("question-text");
-question.innerHTML = "What is the deepest point on Earth?";
+let answer = Array.from(document.getElementsByClassName('answer-text'));
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let availableQuestion = [];
+
+let questions = [
+    {
+        question: "What is the deepest point on Earth?";
+        Answer1: "test1";
+        Answer2: "test2";
+    }
+]
+
+//question.innerHTML = "What is the deepest point on Earth?";
